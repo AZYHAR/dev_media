8e38d08374288937c940bdd75f766264e2face28
Anton Zyhar <azyhar@Antons-MacBook-Pro.local>
2019-07-22 03:07:47 -0400

    Logout & Navbar Links

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,8 +1,6 @@
-// ALERTS
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 
-// REGISTRATION
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 
@@ -11,3 +9,5 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const LOGOUT = 'LOGOUT';
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -49,6 +50,7 @@ export default function(state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       // Remove token
       localStorage.removeItem('token');
       return {
