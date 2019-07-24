213872dfdcfbc1ecd2cd898b9da23d2ac3c1f992
Anton Zyhar <azyhar@Antons-MacBook-Pro.local>
2019-07-23 20:22:22 -0400

    Post Reducer, Action & Initial Component

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddExerience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 // Redux
 // Will combine react + redux
@@ -66,6 +67,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,9 +11,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/profiles'>Developers</Link>
       </li>
       <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />
-          <span className='hide-sm'>Dashboard</span>
+          <span className='hide-sm'> Dashboard</span>
         </Link>
       </li>
       <li>
